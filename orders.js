a3db4d60981d92030bc9ea5184b302ca08f7cb62
siunlucky <faizelfahad2@gmail.com>
2022-07-13 02:36:11 +0700

    Responsive-admin-dashboard

---
+++ orders.js
@@ -0,0 +1,33 @@
+const Orders = [
+    {
+        productName: 'Vandal Prime',
+        productNumber: '08412',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Oni Phamtom',
+        productNumber: '52014',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined'
+    },
+    {
+        productName: 'BattlePass Eps 2 Act 1',
+        productNumber: '82777',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'Prime Phantom 2.0',
+        productNumber: '72431',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'RGX Vandal',
+        productNumber: '62244',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    
+]
